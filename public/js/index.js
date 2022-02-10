87b5340ec2ae5b79f0faa54158270ae9d564591d
Chris Tsang <christopher.tsang@uconn.edu>
2022-02-10 16:45:10 -0500

    complete

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,25 @@ console.log("Hello from index.js!");
 
 
 document.addEventListener("DOMContentLoaded", async () => {
-  document.querySelector("#tweets-container")
-  const tweetsHtml = [  ]
-                  //stringified HTML blocks
+  
+
   try {
     const res = await fetch("http://localhost:8080/tweets");
-    if (res.status == 401) return res.redirected("/log-in")
     const { tweets } = await res.json();
+    const tweetsContainer = document.querySelector("#tweets-container")
+    const tweetsHtml = tweets.map(
+      ({ message }) => `
+      <div class= "card">
+        <div class = "card-body">
+          <p class= "card-text">${message}</p>
+        </div>
+      </div>
+      `
+    );
+    tweetsContainer.innerHTML = tweetsHtml.join("");
+    
+    // if (res.status == 401) return res.redirected("/log-in")
+    
   } catch (e) { console.error(e) };
 
 });
